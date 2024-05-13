1143629cef5720d63c5b9709248df9d288f7f9b5
Ankit Raj <ankitraj7217@gmail.com>
2024-05-14 01:32:25 +0530

    Update debouncing.js

--- debouncing.js
+++ debouncing.js
@@ -24,4 +24,35 @@ check2()
 // only one function is attached. can check from above code.
 input.addEventListener("input", debounce((e) => {
     console.log("event: ", e.target.value);
-},400))
+},400));
+
+
+function debounceWithOptions(fn, delay, option = {leading: false, trailing: false}) {
+    let timeOut;
+    let isLeadingInvoked = false;
+
+    return function(...args) {
+        if (timeOut) {
+            clearTimeout(timeOut);
+        }
+        // Please check this section of logic.
+        if (!timeOut && option.leading) {
+            fn.apply(this, args);
+            isLeadingInvoked = true;
+        } else {
+            isLeadingInvoked = false;
+        }
+
+        timeOut = setTimeout(() => {
+            if (option.trailing && !isLeadingInvoked) {
+                fn.apply(this, args);
+            }
+
+            timeOut = null;
+        }, delay)
+    }
+}
+
+input.addEventListener("input", debounceWithOptions((e) => {
+    console.log("event with options: ", e.target.value);
+},400, {leading: true, trailing: true}));
